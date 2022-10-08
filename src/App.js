95d11d1426527052375b45e7d2e2b4801c941ac0
tonmoycoder <tanverislamtonmoy2929@gmail.com>
2022-10-08 19:14:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-5xl font-bold underline">Hello world!</h1>
+      <div class="p-7 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div class="shrink-0">
+          <img
+            class="h-12 w-12"
+            src="https://www.clipartmax.com/png/full/480-4804984_chit-chat-media-group-energy-sector-icon-png.png"
+            alt="ChitChat Logo"
+          />
+        </div>
+        <div>
+          <div class="text-xl font-bold text-black">ChitChat</div>
+          <p class="text-slate-600 font-medium">You have a new message!</p>
+        </div>
+      </div>
     </div>
   );
 }
