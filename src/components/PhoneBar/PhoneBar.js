6e264fd62eae0d04be7c0c37abc569467f5f4c87
tonmoycoder <tanverislamtonmoy2929@gmail.com>
2022-10-10 08:08:02 +0600

    chart added2

--- src/components/PhoneBar/PhoneBar.js
+++ src/components/PhoneBar/PhoneBar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
 
 
 const PhoneBar = () => {
